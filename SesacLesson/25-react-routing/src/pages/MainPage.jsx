0dca4aa688365859fcca229bbe62cc8f308c41bc
suvd0377 <suvd037@icloud.com>
2025-01-17 16:17:26 +0900

    25.1.17

--- SesacLesson/25-react-routing/src/pages/MainPage.jsx
+++ SesacLesson/25-react-routing/src/pages/MainPage.jsx
@@ -1,7 +1,12 @@
+import { useSearchParams } from 'react-router-dom';
+
 export default function MainPage() {
+  const [searchQuery, setSearchQuery] = useSearchParams();
+  console.log(searchQuery.get('mode')); // Dark, null
   return (
-    <main>
+    <main className={[`MainPage ${searchQuery.get('mode')}`]}>
       <p>여기는 홈입니다! 🏠</p>
+      <button onClick={() => setSearchQuery({ mode: 'Dark' })}>Darkmode</button>
     </main>
   );
 }
